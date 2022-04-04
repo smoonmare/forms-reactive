792d8bd28d2ed7177ea2235048ac0ac339f40860
smoonmare <smoonmare@gmail.com>
2022-04-03 19:14:40 -0700

    Initial commit. Added Bootstrap.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forms-reactive';
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
 }
