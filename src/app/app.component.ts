4196ee38c6e93256eb02daee0c6bb4748829627a
smoonmare <smoonmare@gmail.com>
2022-04-15 22:15:41 -0700

    Added custom email async validator.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -20,7 +21,7 @@ export class AppComponent implements OnInit {
       this.signupForm = new FormGroup({
         'userData': new FormGroup({
           'username': new FormControl(null, [Validators.required, this.isNameForbidden.bind(this)]),
-          'email': new FormControl(null, [Validators.email, Validators.required])
+          'email': new FormControl(null, [Validators.email, Validators.required, this.forbiddenEmails.bind(this)])
         }),
         'gender': new FormControl('male'),
         'hobbies': new FormArray([])
@@ -45,5 +46,18 @@ export class AppComponent implements OnInit {
       return {'nameIsForbidden': true};
     }
     return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
